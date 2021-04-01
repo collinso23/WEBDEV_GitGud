bdb1dea767ea5eccfcb455755e60470e660e2918
Orion Collins <46937911+collinso23@users.noreply.github.com>
2021-04-01 14:08:25 -0400

    invokeBashProcess.py (Sh_with_py) working
    invokeBashProcess will work from the CLI, next we need to get the graphical interfaces up and then call the invokeBashProcess program to execute the desired tool. Since it works with any arugment list we can create the graphical tool to pass args in as string to the program

--- gitgud_web_source/gitgud_web_source/settings.py
+++ gitgud_web_source/gitgud_web_source/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'gitgud_web_source.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',   
+        'PORT': 5432,
     }
 }
 
